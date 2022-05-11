ea5d2effd964e9c3ecb51d3c9930779f1bcf8267
uclabruin <email@example.com>
2022-05-11 04:22:20 -0700

    Added Comments

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
+# The percentage of the population
+# initially infected with the STD.
 INITIAL_INFECTION_RATE = 0.01
+
+# The size of the population.
 POPULATION_SIZE = 1000
+
+# The number of sexual encounters
+# per round of the simulation.
 NUMBER_OF_SEXUAL_ENCOUNTERS = 50
+
+# TODO: Comment on model parameters.
 MAX_ALPHA = 5
 MAX_ABSOLUTE_BETA = 5
